b3c807b5fe74d5182194777c90680e4659958635
NATHAlYNJANE <arienzanathalyn@gmail.com>
2025-05-09 02:30:32 +0800

    Updates

--- assets/ios.transition-legacy-HHR5fsgh.js
+++ assets/ios.transition-legacy-HHR5fsgh.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-hcX0wi6X.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-DkPsE9KB.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
